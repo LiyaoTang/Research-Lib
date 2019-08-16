c9692bbd8792e70c1d4d37295bcef8262fb88282
beihaifusang <beihaifusang@gmail.com>
2019-08-16 15:58:08 +0800

    add torch load

--- Models/Torch_Models/Torch_Ops.py
+++ Models/Torch_Models/Torch_Ops.py
@@ -81,3 +81,55 @@ def weight_l1_loss(pred_loc, label_loc, loss_weight):
     diff = diff.sum(dim=1).view(b, -1, sh, sw)
     loss = diff * loss_weight
     return loss.sum().div(b)
+
+
+''' save & restore '''
+
+
+def check_keys(model, state_dict):
+    '''
+    check coverage between loaded var (state_dict) & model var (model.state_dict())
+    '''
+    ckpt_keys = set(state_dict.keys())
+    model_keys = set(model.state_dict().keys())
+
+    used_pretrained_keys = model_keys & ckpt_keys
+    unused_pretrained_keys = ckpt_keys - model_keys
+    missing_keys = model_keys - ckpt_keys
+    missing_keys = [x for x in missing_keys if not x.endswith('num_batches_tracked')]  # filter 'num_batches_tracked'
+
+    if len(missing_keys) > 0:
+        print('[Warning] missing_keys, num=', len(missing_keys), ':\n', missing_keys)        
+    if len(unused_pretrained_keys) > 0:
+        print('[Warning] unused_pretrained_keys, num=', len(unused_pretrained_keys), ':\n', unused_pretrained_keys)
+    print('used keys', len(used_pretrained_keys))
+    return len(used_pretrained_keys) > 0
+
+def restore(model, save_file, optimizer=None, device='cpu'):
+    if device == 'cpu':
+        ckpt_dict = torch.load(save_file, map_location=lambda storage, loc: storage.cpu())
+    else:  # load into gpu
+        device = torch.cuda.current_device()
+        ckpt_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage.cuda(device))
+
+    # load model
+    state_dict = ckpt_dict['state_dict'] if "state_dict" in state_dict.keys() else ckpt_dict
+    
+    pfx = 'module.'  # remove prefix 'module.' (old style saved model)
+    state_dict = {k[len(pfx):] if k.startswith(pfx) else k: v for k, v in state_dict.items()}
+
+    if not check_keys(model, state_dict):  # check keys in loaded state_dict
+        print('retry after adding prefix \'features.\'')  # retry
+        state_dict = {'features.' + k: v for k, v in state_dict}
+        if not check_keys(model, state_dict):
+            raise AttributeError, 'nothing loaded from save_file=', save_file
+    model.load_state_dict(state_dict, strict=False)
+
+    # load optimizer
+    if 'optimizer' in ckpt_dict and optimizer is not None:
+        if check_keys(optimizer, ckpt['optimizer']):
+            optimizer.load_state_dict(ckpt['optimizer'])
+    
+    epoch = ckpt['epoch'] if 'epoch' in ckpt else 0
+
+    return model, optimizer, epoch
