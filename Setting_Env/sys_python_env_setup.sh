f717e28deef5e4a5b6b6dbb416d32ff7bbff3837
beihaifusang <beihaifusang@gmail.com>
2019-12-13 14:14:04 +0800

    shell script: install env

--- Setting_Env/sys_python_env_setup.sh
+++ Setting_Env/sys_python_env_setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# set up python3 env after changing sys default python from 3.5->3.6
+# set up python3 env after changing sys default python from 3.5 -> 3.6 or greater
 
 a=$(find /usr/lib/python3/dist-packages -name '*35m*so')
 b=$(echo $a | tr 35m 36m)
-IFS=' ' read -r -a a <<< $a
+IFS=' ' read -r -a a <<< $a  # separate string into arrays
 IFS=' ' read -r -a b <<< $b
 
 for ((i=0;i<${#a[@]};++i)); do
+    echo "linking ${a[i]} -> ${b[i]}"
     ln -s "${a[i]}" "${b[i]}"
 done
