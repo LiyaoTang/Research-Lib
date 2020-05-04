3170a7078fece2c72501d8c319813ff0c0a7f2dc
beihaifusang <beihaifusang@gmail.com>
2020-05-04 21:51:06 +0800

    add printProcess

--- Utilities/Dist_Utils.py
+++ Utilities/Dist_Utils.py
@@ -16,3 +16,25 @@ def get_local_addr():
     port = s.getsockname()[1]
     s.close()
     return addr, port
+
+# Print iterations progress (thanks StackOverflow)
+def printProgress(iteration, total, prefix='', suffix='', decimals=1, barLength=100):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        barLength   - Optional  : character length of bar (Int)
+    """
+    import sys
+    formatStr       = "{0:." + str(decimals) + "f}"
+    percents        = formatStr.format(100 * (iteration / float(total)))
+    filledLength    = int(round(barLength * iteration / float(total)))
+    bar             = '' * filledLength + '-' * (barLength - filledLength)
+    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),
+    if iteration == total:
+        sys.stdout.write('\x1b[2K\r')
+    sys.stdout.flush()
