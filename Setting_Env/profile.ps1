a22856ddcb073b168877f7734fc201bc0f307d1e
beihaifusang <beihaifusang@gmail.com>
2022-08-10 01:27:23 +0800

    add win powershell profile

---
+++ Setting_Env/profile.ps1
@@ -0,0 +1,6 @@
+
+# NOTE: to use conda, check https://stackoverflow.com/questions/64149680/how-to-activate-conda-environment-from-powershell
+
+Set-Alias -Name ll -Value Get-ChildItem
+# Set-Alias -Name ls -Value Get-ChildItem  # alread have
+# Set-Alias -Name clear -Value Clear-Host  # alread have
